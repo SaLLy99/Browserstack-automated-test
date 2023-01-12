41495c570ac255e089faad1af773075d6e87e805
SaLLy99 <sally.tkhilaishvili@gmail.com>
2023-01-12 16:22:46 +0400

    Update SeleniumTest.java

--- src/test/java/com/browserstack/SeleniumTest.java
+++ src/test/java/com/browserstack/SeleniumTest.java
@@ -1,10 +1,15 @@
 package com.browserstack;
 
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.net.URL;
+import java.util.HashMap;
 
 public class SeleniumTest {
     public WebDriver driver;
@@ -14,7 +19,23 @@ public class SeleniumTest {
     public void setUp() throws Exception {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
-        driver = new ChromeDriver(options);
+        String username = System.getenv("BROWSERSTACK_USERNAME");
+        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        String buildName = System.getenv("JENKINS_LABEL");
+
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("browserVersion", "100.0");
+        HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
+        browserstackOptions.put("os", "Windows");
+        browserstackOptions.put("osVersion", "10");
+        browserstackOptions.put("sessionName", "BStack Build Name: " + buildName);
+        browserstackOptions.put("seleniumVersion", "4.0.0");
+        capabilities.setCapability("bstack:options", browserstackOptions);
+
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
+
+
     }
 
     @AfterMethod(alwaysRun = true)
